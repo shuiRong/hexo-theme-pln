8f6eafcb6a48e42a8af88968e9ed111d581c890c
shuiRong <shuirong1997@icloud.com>
2019-02-02 22:41:31 +0800

    加一些好玩的东西：看板娘、点击文字特效、下方两角的蕾姆拉姆、替换成VueMarkdown样式

---
+++ source/js/click.js
@@ -0,0 +1,72 @@
+$('document').ready(function() {
+  var a_idx = 0
+  jQuery(document).ready(function($) {
+    $('body').click(function(e) {
+      var a = new Array(
+        '富强',
+        '民主',
+        '文明',
+        '和谐',
+        '自由',
+        '平等',
+        '公正',
+        '法治',
+        '爱国',
+        '敬业',
+        '诚信',
+        '友善'
+      )
+      var $i = $('<span />').text(a[a_idx])
+      a_idx = (a_idx + 1) % a.length
+      var x = e.pageX,
+        y = e.pageY
+      $i.css({
+        'z-index': 999,
+        top: y - 20,
+        left: x,
+        position: 'absolute',
+        'font-weight': 'bold',
+        color: '#d92412',
+        'font-size': '20px'
+      })
+      $('body').append($i)
+      $i.animate(
+        {
+          top: y - 180,
+          opacity: 0
+        },
+        1500,
+        function() {
+          $i.remove()
+        }
+      )
+    })
+  })
+})
+
+var os = (function() {
+  var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isTablet =
+      /(?:iPad|PlayBook)/.test(ua) ||
+      (isAndroid && !/(?:Mobile)/.test(ua)) ||
+      (isFireFox && /(?:Tablet)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian
+  return {
+    isPc: isPc
+  }
+})()
+
+if (!os.isPc) {
+  $('#updown').hide()
+  var setTime = setInterval(function() {
+    if ($('#live2d-widget').length) {
+      $('#live2d-widget').hide()
+      clearInterval(setTime)
+    }
+  }, 200)
+}
